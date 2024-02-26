69ccf1956bf5c999cd7a0a1400926aa71a473bbf
xsni1 <thesniboy@gmail.com>
2024-02-26 22:57:14 +0100

    Start rewriting parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,87 +1,68 @@
 package parser
 
-import (
-	"fmt"
-	"slices"
-	"strconv"
+const (
+	SimpleString   byte = '+'
+	SimpleError    byte = '-'
+	Integer        byte = ':'
+	BulkString     byte = '$'
+	Array          byte = '*'
+	Null           byte = '_'
+	Boolean        byte = '#'
+	Double         byte = ','
+	BigNumber      byte = '('
+	BulkError      byte = '!'
+	VerbatimString byte = '='
+	Map            byte = '%'
+	Set            byte = '~'
+	Push           byte = '>'
 )
 
+// Structured this way to handle packets segmentation of IP protocol
 func Parse(in <-chan []byte) <-chan []string {
 	out := make(chan []string)
 
 	go func() {
-        defer close(out)
+		defer close(out)
 		var (
-			buffer       = []byte{}
-			multiBulkLen = 0
-			elements     []string
+			dataType byte
 		)
 
-		for msg := range in {
-			fmt.Println("Processing: ", msg)
-			buffer = append(buffer, msg...)
-
-			if multiBulkLen == 0 {
-				// Multi bulk - array of bulk strings
-				if buffer[0] == '*' {
-					newlineIdx := slices.Index(buffer, '\r')
-					if newlineIdx == -1 || len(buffer) <= newlineIdx+1 || (len(buffer) > newlineIdx+1 && buffer[newlineIdx+1] != '\n') {
-						continue
-					}
-					c, err := strconv.Atoi(string(buffer[1:newlineIdx]))
-					if err != nil {
-						fmt.Printf("err reading array len: %v\n", err)
-						return
-					}
-					multiBulkLen = c
-					buffer = buffer[newlineIdx+2:]
-				} else {
-					// inline
+		for payload := range in {
+			if dataType == 0 {
+				switch payload[0] {
+				case SimpleString:
+					dataType = SimpleString
+				case SimpleError:
+					dataType = SimpleError
+				case Integer:
+					dataType = Integer
+				case BulkString:
+					dataType = BulkString
+				case Array:
+					dataType = Array
+				case Null:
+					dataType = Null
+				case Boolean:
+					dataType = Boolean
+				case Double:
+					dataType = Double
+				case BigNumber:
+					dataType = BigNumber
+				case BulkError:
+					dataType = BulkString
+				case VerbatimString:
+					dataType = VerbatimString
+				case Map:
+					dataType = Map
+				case Set:
+					dataType = Set
+				case Push:
+					dataType = Push
 				}
 			}
 
-			for multiBulkLen > 0 {
-				if buffer[0] != '$' {
-					fmt.Print("err decoding, expected $\n")
-					return
-				}
-				newlineIdx := slices.Index(buffer, '\r')
-				if newlineIdx == -1 || len(buffer) <= newlineIdx+1 || (len(buffer) > newlineIdx+1 && buffer[newlineIdx+1] != '\n') {
-					break
-				}
-				strLen, err := strconv.Atoi(string(buffer[1:newlineIdx]))
-				if err != nil {
-					fmt.Printf("err reading element len: %v\n", err)
-					return
-				}
-				if len(buffer) <= newlineIdx+2+strLen {
-					break
-				}
-				elements = append(elements, string(buffer[newlineIdx+2:newlineIdx+2+strLen]))
-				buffer = buffer[newlineIdx+2+strLen+2:]
-				multiBulkLen--
-			}
-
-			if multiBulkLen > 0 {
-				continue
-			}
-
-			// execute
-			// res, err := parseCommand(elements)
-			// if err != nil {
-			// 	conn.Write([]byte(fmt.Sprintf("-%s\r\n", err)))
-			// } else {
-			// 	conn.Write([]byte(res))
-			// }
-
-			// could probably reuse existing slices
-			buffer = []byte{}
-			// elements = []string{}
-			// fmt.Println("Parsed: ", elements)
 		}
-		fmt.Println("Parsed: ", elements)
-
-		out <- elements
+		// out <- elements
 	}()
 
 	return out
